4afb1db4ce27174164876b78c0dcb8f775971b1c
WescMC <106628009+wsalguero@users.noreply.github.com>
2023-03-10 12:23:41 -0600

    Delete Control-de-Empresas-1-main directory

--- Control-de-Empresas-1-main/index.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-
-//Importación de configuración de server
-const Server = require('./models/server');
-
-const servidorIniciado = new Server();
-
-servidorIniciado.listen();
